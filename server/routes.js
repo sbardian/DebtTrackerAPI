4ab3a8325cffb36e34bddfb3321097b9cd6f8023
Brian <sbardian@gmail.com>
2016-12-13 19:56:02 -0700

    Comment routes

--- server/routes.js
+++ server/routes.js
@@ -7,42 +7,43 @@ let routesLogic = require('./routesLogic');
 
 module.exports = function(router) {
 
+    /**
+     * Set specific headers.  Allow CORS.
+     */
     router.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next(); // go to next routes
+        next();
     });
 
-    // test route (accessed at GET http://localhost:8080/api)
-    // =============================================================================
+    /**
+     * route ending in "/"
+     */
     router.get('/', function(req, res) {
         res.json({ message: 'API up and running.' });
     });
 
-    // route /creditcards
-    // =============================================================================
+    /**
+     * routes ending in "/creditcards"
+     */
     router.route('/creditcards')
-        // create a credit card (accessed at POST http://localhost:8080/api/creditcards)
         .post(function(req, res){
             routesLogic.addCreditCard(req, res);
         })
-
-        // get all the credit cards (accessed at GET http://localhost:8080/api/creditcards)
         .get(function(req, res) {
             routesLogic.getAllCreditCards(req, res);
         });
 
-    // route /creditcards/id
-    // =============================================================================
+    /**
+     * routes ending in "/creditcards/id"
+     */
     router.route("/creditcards/:id")
         .get(function(req, res) {
             routesLogic.getCreditCardByID(req, res);
         })
-
         .put(function(req, res){
             routesLogic.putOrUpdate(req, res);
         })
-
         .delete(function(req, res){
             routesLogic.deleteCreditCard(req, res);
         });
