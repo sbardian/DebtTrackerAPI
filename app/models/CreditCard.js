67f57485021bbd134372238de8ed3ade0db7018d
Brian <sbardian@gmail.com>
2017-05-05 20:19:36 -0600

    Test using var.

--- app/models/CreditCard.js
+++ app/models/CreditCard.js
@@ -2,14 +2,14 @@
  * Created by sbardian on 12/12/16.
  */
 
-const mongoose = require('mongoose');
-const bluebird = require('bluebird');
+var mongoose = require('mongoose');
+var bluebird = require('bluebird');
 
 mongoose.Promise = bluebird;
 
 mongoose.connect('mongodb://localhost/DeptTracker');
 
-let db = mongoose.connection;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('connected to db');
