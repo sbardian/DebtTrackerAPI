2c031a9eb1dfb80e2274de3fb35ecc0f4d7df11a
Brian <sbardian@gmail.com>
2017-05-05 20:12:37 -0600

    add connectdb back.

--- server/server.js
+++ server/server.js
@@ -5,20 +5,7 @@
 const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
-//const connectDB = require('./connectDB');
-
-const mongoose = require('mongoose');
-const bluebird = require('bluebird');
-
-mongoose.Promise = bluebird;
-
-mongoose.connect('mongodb://localhost/DeptTracker');
-
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('connected to db');
-});
+const connectDB = require('./connectDB');
 
 // configure server to use bodyParser()
 // this will let us get the data from a POST
