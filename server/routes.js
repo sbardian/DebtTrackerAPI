4f3c4d5e431f0d35d9e64fe078d8cf5ab58be133
Brian <sbardian@gmail.com>
2017-05-10 09:36:12 -0600

    Wrap in module for export.

--- server/routes.js
+++ server/routes.js
@@ -4,49 +4,54 @@
 
 const routesLogic = require('./routesLogic');
 
-module.exports = (router) => {
-  /**
-   * Set specific headers.  Allow CORS.
-   */
-  router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    next();
-  });
+const routes = {
+  setRoutes(router) {
+    /**
+     * Set specific headers.  Allow CORS.
+     */
+    router.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      next();
+    });
 
-  /**
-   * route ending in '/'
-   */
-  router.get('/', (req, res) => {
-    res.json({ message: 'API up and running.' });
-  });
+    /**
+     * route ending in '/'
+     */
+    router.get('/', (req, res) => {
+      res.json({ message: 'API up and running.' });
+    });
 
-  /**
-   * routes ending in '/creditcards'
-   */
-  router.route('/creditcards')
-      .post((req, res) => routesLogic.addCreditCard(req, res))
-      .get((req, res) => routesLogic.getAllCreditCards(req, res));
+    /**
+     * routes ending in '/creditcards'
+     */
+    router.route('/creditcards')
+        .post((req, res) => routesLogic.addCreditCard(req, res))
+        .get((req, res) => routesLogic.getAllCreditCards(req, res));
 
-  /**
-   * routes ending in '/creditcards/id'
-   */
-  router.route('/creditcards/:id')
-      .get((req, res) => routesLogic.getCreditCardByID(req, res))
-      .put((req, res) => routesLogic.putOrUpdate(req, res))
-      .delete((req, res) => routesLogic.deleteCreditCard(req, res));
+    /**
+     * routes ending in '/creditcards/id'
+     */
+    router.route('/creditcards/:id')
+        .get((req, res) => routesLogic.getCreditCardByID(req, res))
+        .put((req, res) => routesLogic.putOrUpdate(req, res))
+        .delete((req, res) => routesLogic.deleteCreditCard(req, res));
 
-  /**
-   * routes ending in '/totals'
-   */
-  router.route('/totals')
-      .post((req, res) => routesLogic.addTotal(req, res))
-      .get((req, res) => routesLogic.getTotals(req, res));
+    /**
+     * routes ending in '/totals'
+     */
+    router.route('/totals')
+        .post((req, res) => routesLogic.addTotal(req, res))
+        .get((req, res) => routesLogic.getTotals(req, res));
 
-  /**
-   * routes ending in '/totals/id
-   */
-  router.route('/totals/:id')
-      .delete((req, res) => routesLogic.deleteTotal(req, res));
+    /**
+     * routes ending in '/totals/id
+     */
+    router.route('/totals/:id')
+        .delete((req, res) => routesLogic.deleteTotal(req, res));
+  },
 };
+
+module.exports = routes;
