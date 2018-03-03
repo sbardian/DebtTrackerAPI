5ed3cb2038c8c83b1a62fa09574a0069268482b7
Brian Andrews <sbardian@gmail.com>
2018-03-03 09:07:47 -0700

    Remove user after register test

--- src/server/routesLogic/register.test.js
+++ src/server/routesLogic/register.test.js
@@ -1,7 +1,26 @@
 import request from 'supertest';
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
 import { server } from '../server';
 import { SUCCESS_REGISTER_MOCK_USER } from '../testEnv/fictures';
 
+let db;
+let users;
+
+beforeAll(async () => {
+  mongoose.connect(global.__MONGO_URI__, {
+    useMongoClient: true,
+    promiseLibrary: bluebird,
+  });
+  db = await mongoose.connection;
+  // users = await db.collection('users');
+  // await users.insertOne(LOGIN_MOCK_USER);
+});
+
+afterAll(async () => {
+  await users.removeAll({});
+  await db.close();
+});
 describe('Test Register API paths', () => {
   it('Register success, return 200 status: ', () =>
     request(server)
