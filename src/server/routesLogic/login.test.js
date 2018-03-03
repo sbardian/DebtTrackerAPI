031fd1f21a9b5c9c7583e8e7d5f9785420935271
Brian Andrews <sbardian@gmail.com>
2018-03-03 08:56:22 -0700

    Test register API path

--- src/server/routesLogic/login.test.js
+++ src/server/routesLogic/login.test.js
@@ -2,38 +2,40 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import bluebird from 'bluebird';
 import { server } from '../server';
-import { MOCK_USER } from '../testEnv/fictures';
+import { LOGIN_MOCK_USER } from '../testEnv/fictures';
+
+let db;
+let users;
 
 beforeAll(async () => {
   mongoose.connect(global.__MONGO_URI__, {
     useMongoClient: true,
     promiseLibrary: bluebird,
   });
-
-  const db = await mongoose.connection;
-  const users = await db.collection('users');
-  // await request(server)
-  //   .post('/auth/register')
-  //   .type('form')
-  //   .set('Accept', 'text/html, application/json')
-  //   .send(MOCK_USER)
-  //   .expect(200);
-  await users.insertOne(MOCK_USER);
+  db = await mongoose.connection;
+  users = await db.collection('users');
+  await users.insertOne(LOGIN_MOCK_USER);
 });
 
-describe('Test API paths', () => {
-  describe('Test Login', () => {
-    it('Login success, return 200 status: ', () =>
-      request(server)
-        .post('/auth/login')
-        .set('Accept', 'text/html, application/json')
-        .send({ email: MOCK_USER.email, password: MOCK_USER.passwordConf })
-        .expect(200));
-    it('Login failure, return 401 status: ', () =>
-      request(server)
-        .post('/auth/login')
-        .set('Accept', 'text/html, application/json')
-        .send({ email: MOCK_USER.email, password: 'InvalidPassword' })
-        .expect(401));
-  });
+afterAll(async () => {
+  await users.removeAll({});
+  await db.close();
+});
+
+describe('Test Login API paths', () => {
+  it('Login success, return 200 status: ', () =>
+    request(server)
+      .post('/auth/login')
+      .set('Accept', 'text/html, application/json')
+      .send({
+        email: LOGIN_MOCK_USER.email,
+        password: LOGIN_MOCK_USER.passwordConf,
+      })
+      .expect(200));
+  it('Login failure, return 401 status: ', () =>
+    request(server)
+      .post('/auth/login')
+      .set('Accept', 'text/html, application/json')
+      .send({ email: LOGIN_MOCK_USER.email, password: 'InvalidPassword' })
+      .expect(401));
 });
--- src/server/testEnv/fictures.js
+++ src/server/testEnv/fictures.js
@@ -1,6 +1,23 @@
-export const MOCK_USER = {
+export const LOGIN_MOCK_USER = {
   email: 'test@test.com',
   username: 'test',
   password: '$2a$10$9fEj.goY9ZR1INP8iup/1exbhGBj8BBmOKfUWJwpZmD8.cPF3zrhi',
   passwordConf: 'testpass',
+  isAdmin: false,
+};
+
+export const SUCCESS_REGISTER_MOCK_USER = {
+  email: 'success@success.com',
+  username: 'success',
+  password: 'success',
+  passwordConf: 'success',
+  isAdmin: false,
+};
+
+export const FAILURE_REGISTER_MOCK_USER = {
+  email: 'fail@fail.com',
+  username: 'fail',
+  password: 'failpass',
+  passwordConf: 'failpass2',
+  isAdmin: false,
 };
