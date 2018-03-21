71a591f6e6304808581a1a39610b210c3271b24d
Brian Andrews <sbardian@gmail.com>
2018-03-20 22:53:54 -0600

    Add loging test for is Admin and is not Admin

--- src/server/routesLogic/login.test.js
+++ src/server/routesLogic/login.test.js
@@ -1,14 +1,28 @@
 import session from 'supertest-session';
 import mockingoose from 'mockingoose';
 import { server } from '../server';
-import { LOGIN_SUCCESS_MOCK_USER } from '../testEnv/fixtures';
+import {
+  LOGIN_SUCCESS_MOCK_USER,
+  LOGIN_SUCCESS_MOCK_USER_ADMIN,
+} from '../testEnv/fixtures';
 
 jest.mock('./checkAuth');
 
 describe('Test /login API routes', () => {
   const serverSession = session(server);
 
-  it('Login success, return 200 status: ', async () => {
+  it('Login success is Admin, return 200 status: ', async () => {
+    mockingoose.User.toReturn(LOGIN_SUCCESS_MOCK_USER_ADMIN, 'findOne');
+    const response = await serverSession
+      .post('/auth/login')
+      .set('Accept', 'text/html, application/json')
+      .send({
+        email: LOGIN_SUCCESS_MOCK_USER.email,
+        password: LOGIN_SUCCESS_MOCK_USER.passwordConf,
+      });
+    expect(response.statusCode).toBe(200);
+  });
+  it('Login success not Admin, return 200 status: ', async () => {
     mockingoose.User.toReturn(LOGIN_SUCCESS_MOCK_USER, 'findOne');
     const response = await serverSession
       .post('/auth/login')
