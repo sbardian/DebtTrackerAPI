ff18fd32b55e982e1adffb6bdaaefe9a41131202
Brian Andrews <sbardian@gmail.com>
2018-03-23 00:11:14 -0600

    Material UI login

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import { browserHistory, Link } from 'react-router';
+import CssBaseline from 'material-ui/CssBaseline';
+import TextField from 'material-ui/TextField';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
 import {
-  Form,
-  FormControl,
-  ControlLabel,
-  FormGroup,
-  Col,
-  Checkbox,
-  Button,
-} from 'react-bootstrap';
-import { transparentBg } from '../styles';
+  MuiThemeProvider,
+  createMuiTheme,
+  withStyles,
+} from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Button from 'material-ui/Button';
 import utils from '../utils/utils';
 
-export default class Home extends Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    padding: 0,
+    width: 400,
+  },
+  title: {
+    padding: '40px',
+    paddingTop: '50px',
+    backgroundColor: '#bbff99',
+  },
+  form: {
+    fontSize: '2rem !important',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 235,
+  },
+  button: {
+    margin: '10px',
+  },
+});
+
+const theme = createMuiTheme({
+  typography: {
+    fontSize: 32,
+    // Use the system font over Roboto.
+    fontFamily:
+      '-apple-system,system-ui,BlinkMacSystemFont,' +
+      '"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif',
+  },
+});
+
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -21,7 +63,6 @@ export default class Home extends Component {
       password: '',
       isAdmin: false,
     };
-    this.userSelect = this.userSelect.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.userLogin = this.userLogin.bind(this);
   }
@@ -70,76 +111,67 @@ export default class Home extends Component {
     }
   }
 
-  // Sets the state of the user selected.
-  userSelect(e) {
-    this.setState({
-      user: e.target.value,
-    });
-  }
-
   render() {
+    const { classes } = this.props;
     const { email, password } = this.state;
     return (
-      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
-        <h1>DebtTracker</h1>
-        <p className="lead">Lets get started!</p>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 col-md-offset-4">
-              <Form horizontal onSubmit={this.userLogin}>
-                <FormGroup>
-                  <Col componentClass={ControlLabel} sm={2}>
-                    Email
-                  </Col>
-                  <Col sm={10}>
-                    <FormControl
+      <Grid container className={classes.root}>
+        <MuiThemeProvider theme={theme}>
+          <Grid item xs={12}>
+            <Grid container justify={'center'}>
+              <Paper className={classes.paper}>
+                <Typography
+                  className={classes.title}
+                  variant="headline"
+                  component="h3"
+                >
+                  DebtTracker
+                </Typography>
+                <form className={classes.form}>
+                  <Grid item xs={12}>
+                    <TextField
                       id="email"
-                      name="email"
-                      value={email}
+                      label="Email"
                       type="email"
+                      value={email}
                       placeholder="Email"
                       onChange={this.handleChange}
+                      className={classes.textField}
+                      margin="normal"
                     />
-                  </Col>
-                </FormGroup>
-
-                <FormGroup>
-                  <Col componentClass={ControlLabel} sm={2}>
-                    Password
-                  </Col>
-                  <Col sm={10}>
-                    <FormControl
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
                       id="password"
-                      name="password"
-                      value={password}
+                      label="Password"
                       type="password"
+                      value={password}
                       placeholder="Password"
                       onChange={this.handleChange}
+                      className={classes.textField}
+                      margin="normal"
                     />
-                  </Col>
-                </FormGroup>
-
-                <FormGroup>
-                  <Col smOffset={2} sm={10}>
-                    <Checkbox>Remember me</Checkbox>
-                  </Col>
-                </FormGroup>
-
-                <FormGroup>
-                  <Col smOffset={2} sm={10}>
-                    <Link>
-                      <Button type="submit">Login</Button>
-                    </Link>
-                    <Link to={'/register'}>
-                      <Button type="button">Register</Button>
-                    </Link>
-                  </Col>
-                </FormGroup>
-              </Form>
-            </div>
-          </div>
-        </div>
-      </div>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      variant="raised"
+                      className={classes.button}
+                      onClick={event => this.userLogin(event)}
+                    >
+                      Login
+                    </Button>
+                    <Button variant="raised" className={classes.button}>
+                      Register
+                    </Button>
+                  </Grid>
+                </form>
+              </Paper>
+            </Grid>
+          </Grid>
+        </MuiThemeProvider>
+      </Grid>
     );
   }
 }
+
+export default withStyles(styles)(Home);
