9ae8a317a53afd993aa2a3c95596e501ff66b99b
Brian Andrews <sbardian@gmail.com>
2018-10-09 23:25:07 -0600

    Update to @material-ui/core

--- src/client/components/AddDialog.js
+++ src/client/components/AddDialog.js
@@ -1,13 +1,13 @@
 /* eslint react/prefer-stateless-function: 0 */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Button from 'material-ui/Button';
-import Dialog from 'material-ui/Dialog';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import TextField from 'material-ui/TextField';
-import Typography from 'material-ui/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   appBar: {
--- src/client/components/CreditCardsToolbar.js
+++ src/client/components/CreditCardsToolbar.js
@@ -1,17 +1,17 @@
 /* eslint react/prefer-stateless-function: 0 */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Tooltip from 'material-ui/Tooltip';
-import IconButton from 'material-ui/IconButton';
-import DeleteIcon from 'material-ui-icons/Delete';
-import FilterListIcon from 'material-ui-icons/FilterList';
-import AddCircleOutline from 'material-ui-icons/AddCircleOutline';
-import EditIcon from 'material-ui-icons/Edit';
-import DetailsIcon from 'material-ui-icons/Details';
-import { withStyles } from 'material-ui/styles';
-import { lighten } from 'material-ui/styles/colorManipulator';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
+import EditIcon from '@material-ui/icons/Edit';
+import DetailsIcon from '@material-ui/icons/Details';
+import { withStyles } from '@material-ui/core/styles';
+import { lighten } from '@material-ui/core/styles/colorManipulator';
 import classNames from 'classnames';
 
 const toolbarStyles = theme => ({
