5b814ba15b47e5dac9c01cca06d59c03676d6100
Brian Andrews <sbardian@gmail.com>
2018-10-09 23:43:51 -0600

    Add withRoot for theme

---
+++ src/client/components/withRoot.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// A theme with custom primary and secondary color.
+// It's optional.
+const theme = createMuiTheme({
+  typography: {
+    // Use the system font over Roboto.
+    fontFamily:
+      '-apple-system,system-ui,BlinkMacSystemFont,' +
+      '"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif',
+  },
+  palette: {
+    primary: {
+      light: '#b6ffff',
+      main: '#81d4fa',
+      dark: '#4ba3c7',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
+function withRoot(Component) {
+  function WithRoot(props) {
+    // MuiThemeProvider makes the theme available down the React tree
+    // thanks to React context.
+    return (
+      <MuiThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;
