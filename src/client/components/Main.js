cc4a0f532d93b9af8e518cba307333af55373e36
Brian Andrews <sbardian@gmail.com>
2018-10-10 09:41:36 -0600

    WIP: include MuiThemeProvider at top of tree and get theme using withTheme instead of withRoot

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createMuiTheme({
+  typography: {
+    // Use the system font over Roboto.
+    fontFamily:
+      '-apple-system,system-ui,BlinkMacSystemFont,' +
+      '"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif',
+  },
+  palette: {
+    primary: {
+      light: '#b6ffff',
+      main: '#81d4fa',
+      dark: '#4ba3c7',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000000',
+    },
+  },
+});
 
 const Main = ({ children }) => (
   <div>
-    <div className="main-container">{children}</div>
+    <MuiThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <div className="main-container">{children}</div>
+    </MuiThemeProvider>
   </div>
 );
 
