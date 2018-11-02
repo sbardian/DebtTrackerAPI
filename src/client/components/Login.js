23c1c77ef7308e08151025bc1eaea293d66729a7
Brian Andrews <sbardian@gmail.com>
2018-11-02 12:29:25 -0600

    Hook Login

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { browserHistory } from 'react-router';
 import TextField from '@material-ui/core/TextField';
@@ -40,31 +40,19 @@ const styles = theme => ({
   },
 });
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.userLogin = this.userLogin.bind(this);
-  }
+const Login = ({ classes }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  componentDidMount() {
-    document.body.style.overflowY = 'hidden';
-  }
+  document.body.style.overflowY = 'hidden';
 
-  // User Registration
-  register = () => {
+  const register = () => {
     browserHistory.push({
       pathname: `/register`,
     });
   };
 
-  // User login
-  userLogin(e) {
-    const { email, password } = this.state;
+  const userLogin = e => {
     e.preventDefault();
     utils
       .userLogin(email, password)
@@ -88,99 +76,90 @@ class Login extends Component {
       .catch(err => {
         console.log('error in userLogin ', err);
       });
-  }
+  };
 
   // Updates state based on table cell that was edited.
-  handleChange(e) {
+  const handleChange = e => {
     const { value, id } = e.target;
     switch (id) {
       case 'email':
-        this.setState({ email: value });
+        setEmail(value);
         break;
       case 'password':
-        this.setState({ password: value });
+        setPassword(value);
         break;
       default:
         break;
     }
-  }
+  };
 
-  render() {
-    const { classes } = this.props;
-    const { email, password } = this.state;
-
-    return (
-      <div>
-        <Rain count={30} />
-        <Rain count={30} />
-        <Rain count={30} />
-        <Grid className={classes.root}>
-          <Grid
-            container
-            item
-            xs={12}
-            justify="center"
-            className={classes.container}
-          >
-            <Grid>
-              <Paper className={classes.paper}>
-                <Typography
-                  className={classes.title}
-                  variant="h5"
-                  component="h3"
-                >
-                  DebtTracker
-                </Typography>
-                <form className={classes.form}>
-                  <Grid item xs={12}>
-                    <TextField
-                      id="email"
-                      label="Email"
-                      type="email"
-                      value={email}
-                      placeholder="Email"
-                      onChange={this.handleChange}
-                      className={classes.textField}
-                      margin="normal"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      id="password"
-                      label="Password"
-                      type="password"
-                      value={password}
-                      placeholder="Password"
-                      onChange={this.handleChange}
-                      className={classes.textField}
-                      margin="normal"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Button
-                      variant="contained"
-                      className={classes.button}
-                      onClick={event => this.userLogin(event)}
-                    >
-                      Login
-                    </Button>
-                    <Button
-                      variant="contained"
-                      className={classes.button}
-                      onClick={() => this.register()}
-                    >
-                      Register
-                    </Button>
-                  </Grid>
-                </form>
-              </Paper>
-            </Grid>
+  return (
+    <div>
+      <Rain count={30} />
+      <Rain count={30} />
+      <Rain count={30} />
+      <Grid className={classes.root}>
+        <Grid
+          container
+          item
+          xs={12}
+          justify="center"
+          className={classes.container}
+        >
+          <Grid>
+            <Paper className={classes.paper}>
+              <Typography className={classes.title} variant="h5" component="h3">
+                DebtTracker
+              </Typography>
+              <form className={classes.form}>
+                <Grid item xs={12}>
+                  <TextField
+                    id="email"
+                    label="Email"
+                    type="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={handleChange}
+                    className={classes.textField}
+                    margin="normal"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="password"
+                    label="Password"
+                    type="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={handleChange}
+                    className={classes.textField}
+                    margin="normal"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    className={classes.button}
+                    onClick={event => userLogin(event)}
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    variant="contained"
+                    className={classes.button}
+                    onClick={() => register()}
+                  >
+                    Register
+                  </Button>
+                </Grid>
+              </form>
+            </Paper>
           </Grid>
         </Grid>
-      </div>
-    );
-  }
-}
+      </Grid>
+    </div>
+  );
+};
 
 Login.propTypes = {
   classes: PropTypes.shape().isRequired,
