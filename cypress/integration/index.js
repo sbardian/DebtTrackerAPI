a6fe3f8382c27391115c483848c272a0b973ccbd
Brian Andrews <sbardian@gmail.com>
2018-11-27 14:30:51 -0700

    Write first cypress test and delete examples

---
+++ cypress/integration/index.js
@@ -0,0 +1,11 @@
+describe('Register button test', () => {
+  it('Should load register page', () => {
+    cy.visit('http://localhost:9090')
+      .get('form > :nth-child(3) > :nth-child(2)')
+      .click()
+      .get('.MuiTypography-root-139')
+      .should('have.text', 'Register')
+      .get('.MuiToolbar-root-271 > :nth-child(2)')
+      .should('have.text', 'register');
+  });
+});
