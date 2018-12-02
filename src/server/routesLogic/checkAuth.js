2734b31f5da41e69be8d23a647e51d5834deb999
Brian Andrews <sbardian@gmail.com>
2018-12-01 21:36:48 -0700

    Remove log

--- src/server/routesLogic/checkAuth.js
+++ src/server/routesLogic/checkAuth.js
@@ -1,5 +1,4 @@
 import { config } from '../yargs';
-import { log } from '../utils';
 
 const jwt = require('jsonwebtoken');
 
@@ -8,9 +7,7 @@ export const checkAuth = (req, res, next) => {
     req.headers.authorization.split(' ')[1],
     config.sessionSecret,
     (err, decoded) => {
-      log.debug('decoded, ', decoded);
       if (req.session && req.session.userId) {
-        log.info('session success');
         return next();
       }
       return res.status(401).end();
