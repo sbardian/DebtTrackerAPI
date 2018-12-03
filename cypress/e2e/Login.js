93e61234cc48460abed9536afeec1df67c364a4d
Brian Andrews <sbardian@gmail.com>
2018-12-03 13:38:37 -0700

    Update Login test to register user directly

--- cypress/e2e/Login.js
+++ cypress/e2e/Login.js
@@ -3,21 +3,12 @@ import { getUser } from '../../src/client/utils/mockData';
 describe('Login test', () => {
   it('Should login an existing user', () => {
     const user = getUser();
+    cy.request({
+      url: `${Cypress.config().baseUrl}/auth/register`,
+      method: 'POST',
+      body: user,
+    });
     cy.visit('/')
-      .getByText('Register')
-      .click()
-      .getByLabelText(/email/i)
-      .type(user.email)
-      .getByLabelText(/username/i)
-      .type(user.username)
-      .getByLabelText(/password/i)
-      .type(user.password)
-      .getByLabelText(/confirm password/i)
-      .type(user.password)
-      .getByTestId(/register-button/)
-      .click()
-      .getByTestId(/logout-button/i)
-      .click()
       .getByLabelText(/email/i)
       .type(user.email)
       .getByLabelText(/password/i)
