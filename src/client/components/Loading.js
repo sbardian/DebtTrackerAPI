ab47704873478c8e2404766fd7e5dc2b9eb372df
Brian Andrews <sbardian@gmail.com>
2019-01-14 09:01:01 -0700

    Implement Loading component

---
+++ src/client/components/Loading.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default () => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+    }}
+  >
+    <div
+      style={{
+        fontSize: '24pt',
+      }}
+    >
+      Loading. . .
+    </div>
+  </div>
+);
