dffabeb4d22d19f612d48259c0a4d9f18cb6e518
Brian Andrews <sbardian@gmail.com>
2019-01-24 20:55:28 -0700

    Move AddDialog to hooks

--- src/client/components/AddDialog.js
+++ src/client/components/AddDialog.js
@@ -1,5 +1,5 @@
 /* eslint react/prefer-stateless-function: 0 */
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -31,178 +31,178 @@ const styles = theme => ({
   },
 });
 
-class AddDialog extends Component {
-  constructor() {
-    super();
-    this.state = {
-      _id: '',
-      userId: '',
-      name: '',
-      balance: '',
-      limit: '',
-      interest_rate: '',
-      test: '',
-    };
-  }
+const AddDialog = ({
+  cardToEdit,
+  onSave,
+  onClose,
+  onRequired,
+  classes,
+  dialogOpen,
+  onTransition,
+  title,
+}) => {
+  const [_id, setId] = useState('');
+  const [userId, setUserId] = useState('');
+  const [name, setName] = useState('');
+  const [balance, setBalance] = useState('');
+  const [limit, setLimit] = useState('');
+  const [interestRate, setInterestRate] = useState('');
 
-  componentWillReceiveProps(props) {
-    const {
-      _id,
-      userId,
-      name,
-      balance,
-      limit,
-      interest_rate,
-    } = props.cardToEdit;
-    this.setState({
-      _id,
-      userId,
-      name,
-      balance,
-      limit,
-      interest_rate,
-    });
-  }
+  useEffect(
+    () => {
+      const {
+        _id: cteId,
+        userId: cteUserId,
+        name: cteName,
+        balance: cteBalance,
+        limit: cteLimit,
+        interest_rate: cteInterestRate,
+      } = cardToEdit;
+      setId(cteId);
+      setUserId(cteUserId);
+      setName(cteName);
+      setBalance(cteBalance);
+      setLimit(cteLimit);
+      setInterestRate(cteInterestRate);
+    },
+    [cardToEdit],
+  );
 
-  handleChange = (name, event) => {
-    this.setState({
-      [name]: event.target.value,
-    });
+  const handleChange = event => {
+    const { value, id } = event.target;
+    switch (id) {
+      case 'name':
+        setName(value);
+        break;
+      case 'balance':
+        setBalance(value);
+        break;
+      case 'limit':
+        setLimit(value);
+        break;
+      case 'interest_rate':
+        setInterestRate(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  save = () => {
-    const { onSave, onClose, onRequired } = this.props;
-    const { name, limit, balance, interest_rate } = this.state;
-
+  const save = () => {
     // TODO: find a better way to check this.
-    if (!name || !limit || !balance || !interest_rate) {
+    if (!name || !limit || !balance || !interestRate) {
       onRequired();
     } else if (
       name === '' ||
       limit === '' ||
       balance === '' ||
-      interest_rate === ''
+      interestRate === ''
     ) {
       onRequired();
     } else {
-      onSave(this.state);
-      this.setState({
-        _id: '',
-        userId: '',
-        name: '',
-        balance: '',
-        limit: '',
-        interest_rate: '',
-      });
+      onSave({ _id, name, limit, balance, interest_rate: interestRate });
+      setId('');
+      setUserId('');
+      setName('');
+      setBalance('');
+      setLimit('');
+      setInterestRate('');
       onClose();
     }
   };
 
-  close = () => {
-    this.setState({
-      _id: '',
-      userId: '',
-      name: '',
-      balance: '',
-      limit: '',
-      interest_rate: '',
-    });
-    const { onClose } = this.props;
+  const close = () => {
+    setId('');
+    setUserId('');
+    setName('');
+    setBalance('');
+    setLimit('');
+    setInterestRate('');
     onClose();
   };
 
-  render() {
-    const { classes, dialogOpen, onClose, onTransition, title } = this.props;
-
-    const { name, limit, balance, interest_rate } = this.state;
-
-    return (
-      <Dialog
-        fullScreen
-        open={dialogOpen}
-        onClose={onClose}
-        TransitionComponent={onTransition}
-      >
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" color="inherit" className={classes.flex}>
-              {title}
-            </Typography>
-            <Button
-              data-testid="save-card-button"
-              color="inherit"
-              onClick={() => this.save()}
-            >
-              save
-            </Button>
-            <Button
-              color="inherit"
-              onClick={() => this.close()}
-              aria-label="Close"
-            >
-              cancel
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <ValidatorForm className={classes.formContainer} onSubmit={() => {}}>
-          <TextValidator
-            id="name"
-            label="Name"
-            onChange={event => this.handleChange('name', event)}
-            name="name"
-            className={classes.textField}
-            value={name || ''}
-            margin="normal"
-            validators={['required', 'isString']}
-            errorMessages={['Required', 'Name of the credit card.']}
-          />
-          <TextValidator
-            id="limit"
-            label="Limit"
-            onChange={event => this.handleChange('limit', event)}
-            name="limit"
-            className={classes.textField}
-            value={limit || ''}
-            margin="normal"
-            validators={['required', 'isFloat']}
-            errorMessages={[
-              'Required',
-              'Limit for the card.  (e.g. 1500.00 or 1500)',
-            ]}
-          />
-          <TextValidator
-            id="balance"
-            label="Balance"
-            onChange={event => this.handleChange('balance', event)}
-            name="balance"
-            className={classes.textField}
-            value={balance || ''}
-            margin="normal"
-            validators={['required', 'isFloat']}
-            errorMessages={[
-              'Required',
-              'Balance for the card.  (e.g. 500.00 or 500)',
-            ]}
-          />
-          <TextValidator
-            id="interest_rate"
-            label="Interest Rate"
-            onChange={event => this.handleChange('interest_rate', event)}
-            name="interest_rate"
-            className={classes.textField}
-            value={interest_rate || ''}
-            margin="normal"
-            validators={['required', 'isFloat']}
-            errorMessages={[
-              'Required',
-              'Interest rate for the card.  (e.g. 15.0 or 15)',
-            ]}
-          />
-        </ValidatorForm>
-      </Dialog>
-    );
-  }
-}
+  return (
+    <Dialog
+      fullScreen
+      open={dialogOpen}
+      onClose={onClose}
+      TransitionComponent={onTransition}
+    >
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.flex}>
+            {title}
+          </Typography>
+          <Button
+            data-testid="save-card-button"
+            color="inherit"
+            onClick={() => save()}
+          >
+            save
+          </Button>
+          <Button color="inherit" onClick={() => close()} aria-label="Close">
+            cancel
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <ValidatorForm className={classes.formContainer} onSubmit={() => {}}>
+        <TextValidator
+          id="name"
+          label="Name"
+          onChange={handleChange}
+          name="name"
+          className={classes.textField}
+          value={name || ''}
+          margin="normal"
+          validators={['required', 'isString']}
+          errorMessages={['Required', 'Name of the credit card.']}
+        />
+        <TextValidator
+          id="limit"
+          label="Limit"
+          onChange={handleChange}
+          name="limit"
+          className={classes.textField}
+          value={limit || ''}
+          margin="normal"
+          validators={['required', 'isFloat']}
+          errorMessages={[
+            'Required',
+            'Limit for the card.  (e.g. 1500.00 or 1500)',
+          ]}
+        />
+        <TextValidator
+          id="balance"
+          label="Balance"
+          onChange={handleChange}
+          name="balance"
+          className={classes.textField}
+          value={balance || ''}
+          margin="normal"
+          validators={['required', 'isFloat']}
+          errorMessages={[
+            'Required',
+            'Balance for the card.  (e.g. 500.00 or 500)',
+          ]}
+        />
+        <TextValidator
+          id="interest_rate"
+          label="Interest Rate"
+          onChange={handleChange}
+          name="interest_rate"
+          className={classes.textField}
+          value={interestRate || ''}
+          margin="normal"
+          validators={['required', 'isFloat']}
+          errorMessages={[
+            'Required',
+            'Interest rate for the card.  (e.g. 15.0 or 15)',
+          ]}
+        />
+      </ValidatorForm>
+    </Dialog>
+  );
+};
 
 export default withStyles(styles)(AddDialog);
 
