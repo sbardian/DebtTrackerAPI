4082fa9e5a53f42530cc1028d087d6cf060e0300
Brian Andrews <sbardian@gmail.com>
2019-02-05 19:41:25 -0700

    Remove unused import

--- src/client/components/PayOffDetails.js
+++ src/client/components/PayOffDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import Typography from '@material-ui/core/Typography';
