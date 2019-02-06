8a62f3495a8499fd4f008c7cf735e1ff4a527769
Brian Andrews <sbardian@gmail.com>
2019-02-05 19:29:20 -0700

    Confirm session in checkAuth

--- src/server/routesLogic/checkAuth.js
+++ src/server/routesLogic/checkAuth.js
@@ -3,14 +3,14 @@ import { config } from '../yargs';
 const jwt = require('jsonwebtoken');
 
 export const checkAuth = (req, res, next) => {
-  jwt.verify(
-    req.headers.authorization.split(' ')[1],
-    config.sessionSecret,
-    () => {
+  if (req.session && req.session.token) {
+    jwt.verify(req.session.token, config.sessionSecret, () => {
       if (req.session && req.session.userId) {
         return next();
       }
       return res.status(401).end();
-    },
-  );
+    });
+  } else {
+    return res.status(401).end();
+  }
 };
