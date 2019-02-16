f60d72500e93b5f6320c79f24fc7fabdfe1dcfca
Brian Andrews <sbardian@gmail.com>
2019-02-15 21:47:57 -0700

    Attach isAdmin to req.session

--- src/server/routesLogic/checkAuth.js
+++ src/server/routesLogic/checkAuth.js
@@ -1,11 +1,13 @@
 import { config } from '../yargs';
+import { read } from 'fs';
 
 const jwt = require('jsonwebtoken');
 
 export const checkAuth = (req, res, next) => {
   if (req.session && req.session.token) {
-    jwt.verify(req.session.token, config.sessionSecret, () => {
+    jwt.verify(req.session.token, config.sessionSecret, (err, v) => {
       if (req.session.userId) {
+        req.session.isAdmin = v.isAdmin;
         return next();
       }
       return res.status(401).end();
