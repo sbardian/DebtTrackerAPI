c0b9edb529bbe426b3951132d35ee0dca3552627
Brian Andrews <sbardian@gmail.com>
2019-02-16 14:37:02 -0700

    Add getAllUsers to client utils

--- src/client/utils/utils.js
+++ src/client/utils/utils.js
@@ -1,11 +1,13 @@
 const API_BASE_URL = `/api/`;
 const AUTH_BASE_URL = `/auth/`;
+const ADMIN_BASE_URL = `/admin/`;
 const CREDITCARDS_URL = `${API_BASE_URL}creditcards/`;
 const CREDITCARDBYID_URL = `${API_BASE_URL}creditcards/`;
 const TOTALS_URL = `${API_BASE_URL}totals/`;
 const REGISTER_URL = `${AUTH_BASE_URL}register/`;
 const LOGIN_URL = `${AUTH_BASE_URL}login/`;
 const LOGOUT_URL = `${AUTH_BASE_URL}logout/`;
+const GET_ALL_USERS_URL = `${ADMIN_BASE_URL}getAllUsers/`;
 const POST = 'POST';
 const GET = 'GET';
 const PUT = 'PUT';
@@ -75,6 +77,10 @@ const utils = {
 
   // Delete a total
   deleteTotals: id => _fetch(`${TOTALS_URL}${id}`, DELETE),
+
+  // **Admin Only!** get all users
+  getAllUsers: (field, sort) =>
+    _fetch(`${GET_ALL_USERS_URL}?field=${field}&sort=${sort}`),
 };
 
 module.exports = utils;
