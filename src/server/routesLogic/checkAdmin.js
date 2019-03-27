861d95669940fcf9236eb1069042141340b51aca
Brian Andrews <sbardian@gmail.com>
2019-03-26 21:29:13 -0600

    Refactor checkAdmin

--- src/server/routesLogic/checkAdmin.js
+++ src/server/routesLogic/checkAdmin.js
@@ -1,16 +1,14 @@
+import jwt from 'jsonwebtoken';
 import { config } from '../yargs';
 
-const jwt = require('jsonwebtoken');
-
 export const checkAdmin = (req, res, next) => {
-  if (req.session && req.session.token) {
-    jwt.verify(req.session.token, config.sessionSecret, (err, payload) => {
-      if (req.session.userId && payload.isAdmin) {
-        return next();
-      }
-      return res.status(401).end();
-    });
-  } else {
+  if (!req.session || !req.session.token) {
     return res.status(401).end();
   }
+  jwt.verify(req.session.token, config.sessionSecret, (err, payload) => {
+    if (req.session.userId && payload.isAdmin) {
+      return next();
+    }
+    return res.status(401).end();
+  });
 };
