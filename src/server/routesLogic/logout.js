787c841a96ce570e1fe2f6152ac22bfeb91aea59
Brian Andrews <sbardian@gmail.com>
2019-03-26 21:44:32 -0600

    Update logout messages

--- src/server/routesLogic/logout.js
+++ src/server/routesLogic/logout.js
@@ -4,11 +4,11 @@ export const logout = (req, res) => {
       if (err) {
         return res
           .status(400)
-          .json({ error: true, message: 'Error logging out.' });
+          .json({ error: true, message: 'Error logging out' });
       }
       return res
         .status(200)
-        .json({ error: false, message: 'You have successfully logged out.' })
+        .json({ error: false, message: 'Logout successful' })
         .end();
     });
   }
